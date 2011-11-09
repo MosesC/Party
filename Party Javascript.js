a779b2f9bb5c9ec237be5e18743f87b525d95557
Moses Charles <mcharlesjr@yahoo.com>
2011-11-08 19:00:26 -0500

    second commit, im importing all of my files to the repo, im not sure which webpage im gonna use yet, for now ima combine both

---
+++ Party Javascript.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function getDate(){
+    //Date() is a function that comes with javascript
+    var now = new Date();
+    var month = Date.getMonth()
+}
+document.write(month);
